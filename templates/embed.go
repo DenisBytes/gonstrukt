a0613b3450ddddf14b7e10e8ad0eaabb24855ead
DenisBytes <denisbytes@gmail.com>
2026-03-19 12:04:17 +0100

    feat: add k8s manifests, tenancy frontend, and MFA/OAuth routes

--- templates/embed.go
+++ templates/embed.go
@@ -5,5 +5,5 @@ import "embed"
 // FS contains all embedded template files
 // Note: all: is used to include directories starting with _ (like __tests__)
 //
-//go:embed all:common all:gateway all:auth all:database all:static all:frontend
+//go:embed all:common all:gateway all:auth all:database all:static all:frontend all:k8s
 var FS embed.FS
